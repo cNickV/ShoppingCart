ed5a0dcd9359abe75a1daf36cde5d69569edb1d0
Nicolas <nivemo035@hotmail.com>
2022-02-02 10:14:23 -0500

    agrego dinamico de precios

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const cart = document.querySelector("#cart");
 const template = document.querySelector("#template");
+const footer = document.querySelector("#footer");
+const templateFooter = document.querySelector("#templateFooter");
 const fragment = document.createDocumentFragment();
-const btns = document.querySelectorAll(".card .btn");
+
+document.addEventListener("click", (e) => {
+  console.log(e.target.matches(".card .btn-outline-primary"));
+  if (e.target.matches(".card .btn-outline-primary")) {
+    addCart(e);
+  }
+});
 
 const cartObject = [];
 
@@ -10,29 +18,29 @@ const addCart = (e) => {
     nameProduct: e.target.dataset.fruit,
     id: e.target.dataset.fruit,
     amount: 1,
+    cost: parseInt(e.target.dataset.cost),
   };
 
   const index = cartObject.findIndex((item) => item.id === product.id);
-
-  console.log(index);
 
   if (index === -1) {
     cartObject.push(product);
   } else {
     cartObject[index].amount++;
+    cartObject[index].cost = cartObject[index].amount * product.cost;
   }
 
-  createCart(cartObject);
-  console.log(cartObject);
+  createCart();
 };
 
-const createCart = (array) => {
+const createCart = () => {
   cart.textContent = " ";
 
-  array.forEach((item) => {
-    const clone = template.content.firstElementChild.cloneNode(true);
-    clone.querySelector(".lead").textContent = item.nameProduct;
+  cartObject.forEach((item) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector(".text-white .lead").textContent = item.nameProduct;
     clone.querySelector(".badge").textContent = item.amount;
+    clone.querySelector("div .lead span").textContent = item.cost;
 
     fragment.appendChild(clone);
   });
