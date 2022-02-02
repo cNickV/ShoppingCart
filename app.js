1b446912fe61e30a7a3771a002fc633b6f45185d
Nicolas <nivemo035@hotmail.com>
2022-02-02 10:49:53 -0500

    se agregó funcionalidad para agregar o quitar elementos

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const footer = document.querySelector("#footer");
 const templateFooter = document.querySelector("#templateFooter");
 const fragment = document.createDocumentFragment();
 
+/* EVENTS */
 document.addEventListener("click", (e) => {
-  console.log(e.target.matches(".card .btn-outline-primary"));
   if (e.target.matches(".card .btn-outline-primary")) {
     addCart(e);
   }
+  if (e.target.matches(".list-group-item .btn-success")) {
+    btnAdd(e);
+  }
+
+  if (e.target.matches(".list-group-item .btn-danger")) {
+    btnDecrease(e);
+  }
 });
 
-const cartObject = [];
+/*  */
+let cartObject = [];
 
 const addCart = (e) => {
   const product = {
@@ -27,7 +35,6 @@ const addCart = (e) => {
     cartObject.push(product);
   } else {
     cartObject[index].amount++;
-    cartObject[index].cost = cartObject[index].amount * product.cost;
   }
 
   createCart();
@@ -40,12 +47,40 @@ const createCart = () => {
     const clone = template.content.cloneNode(true);
     clone.querySelector(".text-white .lead").textContent = item.nameProduct;
     clone.querySelector(".badge").textContent = item.amount;
-    clone.querySelector("div .lead span").textContent = item.cost;
+    clone.querySelector("div .lead span").textContent = item.cost * item.amount;
+    clone.querySelector(".btn-danger").dataset.id = item.id;
+    clone.querySelector(".btn-success").dataset.id = item.id;
 
     fragment.appendChild(clone);
   });
 
   cart.appendChild(fragment);
+};
+
+const btnAdd = (e) => {
+  cartObject = cartObject.map((item) => {
+    if (item.id === e.target.dataset.id) {
+      item.amount++;
+    }
+    return item;
+  });
+
+  createCart();
+};
+
+const btnDecrease = (e) => {
+  cartObject = cartObject.filter((item) => {
+    if (item.id === e.target.dataset.id) {
+      if (item.amount > 0) {
+        item.amount--;
+        if (item.amount === 0) return;
+        return item;
+      }
+    } else {
+      return item;
+    }
+  });
+  createCart();
 };
 
 btns.forEach((btn) => {
